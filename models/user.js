666cb110b90bf8b327849d14553946bf3648e5b0
Vai Wilmintje <Vai Wilmintje>
2016-08-25 05:19:40 +0700

    add role and user models

---
+++ models/user.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var refId = mongoose.Schema.Types.ObjectId;
+
+var model = new Schema({
+   id: {type: Number, default: null},
+   name: {type: String, required: true},
+   userName: {type: String, required: true},
+   hash: {type: String, required: true},
+   salt: {type: String, required: true},
+   location: {type: refId, ref: 'Location'},
+   role: {type: refId, ref: 'Role'}
+}, {versionKey: false, collection: 'users'});
+
+module.exports = mongoose.model('User', model);
